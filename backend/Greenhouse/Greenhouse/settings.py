5271787d3434dd5e3a7411f45fff17e1e2d0c75a
Icarus-afk <ahmed.ehsan1258@gmail.com>
2024-08-01 18:10:39 +0600

    JWT fix

--- backend/Greenhouse/Greenhouse/settings.py
+++ backend/Greenhouse/Greenhouse/settings.py
@@ -52,11 +52,6 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-}
 
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
@@ -64,20 +59,32 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5500",
 ]
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'ROTATE_REFRESH_TOKENS': False,
+    'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'ALGORITHM': 'HS256',
-    'SIGNING_KEY': SECRET_KEY,
+    'SIGNING_KEY': settings.SECRET_KEY,
     'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
     'AUTH_HEADER_TYPES': ('Bearer',),
     'USER_ID_FIELD': 'id',
     'USER_ID_CLAIM': 'user_id',
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
     'TOKEN_TYPE_CLAIM': 'token_type',
     'JTI_CLAIM': 'jti',
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
 }
 
 CSRF_COOKIE_SECURE = False
--- backend/Greenhouse/User/urls.py
+++ backend/Greenhouse/User/urls.py
@@ -1,13 +1,14 @@
 # urls.py
 
 from django.urls import path
-from .views import login_view, logout_view, refresh_token_view, signup_view, get_user_info_view, set_user_info_view
+from .views import login_view, logout_view, signup_view, get_user_info_view, set_user_info_view
+from rest_framework_simplejwt.views import TokenRefreshView
 
 
 urlpatterns = [
     path("login/", login_view, name="login"),
     path("logout/", logout_view, name="logout"),
-    path("refresh/", refresh_token_view, name="token_refresh"),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path("signup/", signup_view, name="signup"),
     path("info/", get_user_info_view, name="get_user_info"),
     path("update/", set_user_info_view, name="set_user_info"),
